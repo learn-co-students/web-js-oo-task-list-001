7f51bff735f73fe8a3d6494fe536a47b298955cf
Garett Arrowood <garettarrowood@gmail.com>
2015-11-28 20:44:18 -0500

    Done.

--- js/controllers/lists.controller.js
+++ js/controllers/lists.controller.js
@@ -1,3 +1,39 @@
 'use strict';
 // Lists Controller
+function ListsController(){
+  this.$addListForm = $('#add_list');
+  this.$listTitleInput = $('#list_title');
+  this.$selectListMenu = $('#select_list');
+  this.$addTaskForm = $('#add_task');
+  this.$wrapper = $('#wrapper');
+}
 
+ListsController.prototype.hideTaskForm = function(){
+  this.$addTaskForm.hide();
+}
+
+ListsController.prototype.addListFormListener = function() {
+  var self = this;
+  this.$addListForm.submit(function(e) {
+    e.preventDefault();
+    new List(self.$listTitleInput.val()).build();
+    self.$addTaskForm.show();
+    self.$listTitleInput.val('');
+  });
+};
+
+ListsController.prototype.destroyListLiveEventListener = function(){
+  var self = this;
+  this.$wrapper.on('click', '.destroy-list', function(){ 
+    var listId = parseInt($(this).parents('h2').next('ul').data('id'));
+    List.all.splice(listId, 1, null);
+    self.$selectListMenu.find('option[value="'+listId+'"]').remove();
+    $(this).parents('.list').remove();
+  });
+};
+
+ListsController.prototype.init = function() {
+  this.hideTaskForm();
+  this.addListFormListener();
+  this.destroyListLiveEventListener();
+};
--- js/controllers/tasks.controller.js
+++ js/controllers/tasks.controller.js
@@ -1,3 +1,38 @@
 'use strict';
-// Tasks Controller
+function TasksController(){
+  this.$addTaskForm = $('#add_task');
+  this.$selectListMenu = $('#select_list');
+  this.$taskDescriptionInput = $('#task_description');
+  this.$taskPriorityInput = $('#task_priority');
+  this.$wrapper = $('#wrapper');
+}
+
+TasksController.prototype.addTaskFormListener = function(){
+  var self = this;
+  this.$addTaskForm.submit(function(event) {
+    event.preventDefault();
+    var listId = parseInt(self.$selectListMenu.val()),
+        taskDescription = self.$taskDescriptionInput.val(),
+        taskPriority = self.$taskPriorityInput.val(),
+        task = new Task(taskDescription, taskPriority, List.all[listId]);
+    task.build();
+    self.$taskDescriptionInput.val('');
+    self.$taskPriorityInput.val('');
+  });
+};
+
+TasksController.prototype.destroyTaskListener = function(){
+  this.$wrapper.on('click', '.destroy-task', function(){ 
+    var listId = parseInt($(this).parents('ul').data('id')),
+        taskId = parseInt($(this).parent('li').data('id')),
+        list = List.all[listId];
+        list.tasks.splice(taskId, 1, null);
+    $(this).parent('li').remove();
+  });
+};
+
+TasksController.prototype.init = function(){
+  this.addTaskFormListener();
+  this.destroyTaskListener();
+}
 
